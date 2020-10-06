90752916a30715039e8ac0134a7b893c3665fecd
Charly98cma <1998cma@gmail.com>
2020-10-06 22:30:59 +0200

    Primeras comprobaciones

---
+++ scripts/configurar_cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Easy error printing function
+echoerr() { echo "$@" 1>&2; }
+
+# Argument check
+if [[ "$#" -ne 1 ]]
+then
+    echoerr "Use: $0 fichero_configuracion"
+    exit
+fi
+
+# File check
+if [[ ! -e "$1" ]]
+then
+    echoerr "El archivo '$1' no existe."
+    exit
+fi
