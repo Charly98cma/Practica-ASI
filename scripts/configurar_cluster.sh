27d8f38e2831eabe2def9b7e4d0f05f1ce40690e
Charly98cma <1998cma@gmail.com>
2020-10-08 23:17:47 +0200

    Comprobaciones y lectura de formato
    - Comprobación del formato de las lineas de configuracion.
    - Ignorados las lineas que son comentarios
    - Lectura de cada campo de configuracion
    - Contador de lineas para los errores

--- scripts/configurar_cluster.sh
+++ scripts/configurar_cluster.sh
@@ -4,17 +4,40 @@
 echoerr() { echo "$@" 1>&2; }
 
 # Argument check
-if [[ "$#" -ne 1 ]]
-then
+if [[ "$#" -ne 1 ]]; then
     echoerr "Use: $0 fichero_configuracion"
-    exit
+    exit -1
 fi
 
 # File check
-if [[ ! -e "$1" ]]
-then
+if [[ ! -e "$1" ]]; then
     echoerr "El archivo '$1' no existe."
-    exit
+    exit -1
 fi
 
-filename = "$1"
+# Name of the config file
+FILE="$1"
+LINE=1
+while read line; do
+    # String --> Array of words
+    IFS=" " read -ra args <<< "$line"
+
+    # Skip the line if it's a comment
+    if [[ "${args[0]}" == "#" ]]; then
+	continue;
+    fi
+
+    # Check for wrong line format
+    if [[ "${#args[@]}" -ne 3 ]]; then
+	echoerr "$0: línea $LINE: Error de formato en linea de configuracion"
+	exit -1
+    fi
+
+    DIR="${args[0]}"
+    SERV="${args[1]}"
+    CONFIG="${args[2]}"
+
+
+    LINE=$(($LINE + 1))
+done < $FILE
+# Length of the array --> ${#args[@]}
