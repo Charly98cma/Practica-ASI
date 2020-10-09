b47735c6b33f15c0a6a19b55de6d82a48701a9af
Charly98cma <1998cma@gmail.com>
2020-10-09 17:28:48 +0200

    Cambiada lectura de fichero y añadidas comprobaciones
    - Se ha cambiado la forma en la que se leen las líneas (ahora las líneas vacías también se cuentan, para poder dar el número de línea correcto en caso de error)
    - Se ha añadido la comprobación de línea vacía

--- scripts/configurar_cluster.sh
+++ scripts/configurar_cluster.sh
@@ -9,33 +9,47 @@ if [[ "$#" -ne 1 ]]; then
     exit -1
 fi
 
-# File check
-if [[ ! -e "$1" ]]; then
-    echoerr "El archivo '$1' no existe."
+# Check if config file exists
+# TODO: See if it's required to test also if it's a file or a dir
+if [[ ! -e "$@" ]]; then
+    echoerr "El archivo '$1' no existe"
     exit -1
 fi
 
 # Name of the config file
 FILE="$1"
 LINE=1
+
+# Reading loop
 while read line; do
     # String --> Array of words
-    IFS=" " read -ra args <<< "$line"
+    IFS=" " read -a args <<< "$line"
 
-    # Skip the line if it's a comment
-    if [[ "${args[0]}" == "#" ]]; then
+    # Skip the line if it's a comment or an empty line
+    if [[ "${args[0]}" == "#" || "${args[0]}" == "" ]]; then
+	LINE=$(($LINE + 1))
 	continue;
     fi
 
     # Check for wrong line format
     if [[ "${#args[@]}" -ne 3 ]]; then
-	echoerr "$0: línea $LINE: Error de formato en linea de configuracion"
+	echoerr "$FILE: linea $LINE: Error de formato en linea de configuracion"
 	exit -1
     fi
 
+    # Configuration parameters
     DIR="${args[0]}"
     SERV="${args[1]}"
     CONFIG="${args[2]}"
+
+    # Check if config file for that service exists
+    if [[ ! -e "$CONFIG" ]]; then
+	echoerr "$FILE: linea $LINE: El archivo '$CONFIG' no existe"
+	exit -1
+    fi
+
+
+
 
 
     LINE=$(($LINE + 1))
