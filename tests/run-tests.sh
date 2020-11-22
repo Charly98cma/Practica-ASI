df54a898b08bd2d49570492d1c6dfd4448517ee2
Charly98cma <1998cma@gmail.com>
2020-11-22 18:21:38 +0100

    Added new testing function

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,47 +1,31 @@
 #!/bin/bash
 
-CMD="./configurar_cluster.sh"
+# Basic command
+CMD="./configurar_cluster.sh";
 
+# Argument of each tests
+TESTS=(
+    ""
+    "patata.txt"
+    "fichero-config--wrongformat.txt"
+    "fichero-config--confignoexiste.txt"
+)
 
-# Test 1 - No argument
-echo -n "Test 1... "
-eval "$CMD &> /dev/null"
-if [[ "$?" -ne 1 ]]; then
-    echo "FAILED!"
-    echo "Error - No pasar fichero de config no genera error"
-    exit 1
-fi
-echo "SUCCESS"
+# Expected results of each test
+TEST_EXRES=(
+    1
+    2
+    3
+    4
+)
 
-
-# Test 2 - Argument file doesnt exist
-echo -n "Test 2... "
-eval "$CMD patata.txt &> /dev/null"
-if [[ "$?" -ne 2 ]]; then
-    echo "FAILED!"
-    echo "Error - Pasar un fichero inexistente no genera error"
-    exit
-fi
-echo "SUCCESS"
-
-
-# Test 3 - Format error on a configuration line
-echo -n "Test 3... "
-eval "$CMD fichero-config--wrongformat.txt &> /dev/null"
-if [[ "$?" -ne 3 ]]; then
-    echo "FAILED!"
-    echo "Error - Un error en el formato de una linea del fichero de configuracion no genera error"
-    exit
-fi
-echo "SUCCESS"
-
-
-# Test 4 - One of the config files of a service doesnt exist
-echo -n "Test 4... "
-eval "$CMD fichero-config--confignoexiste.txt &> /dev/null"
-if [[ "$?" -ne 4 ]]; then
-    echo "FAILED!"
-    echo "Error - Que un fichero de config de un servicio no exista no genera error"
-    exit
-fi
-echo "SUCCESS"
+# Main loop
+for (( i=0; i<"${#TESTS[@]}"; i++ )); do
+    echo -n "Test $i... ";
+    eval "$CMD ${TESTS[i]} &> /dev/null";
+    if [[ "$?" -ne "${TEST_EXRES[i]}" ]]; then
+	echo "FAILED! --> $CMD ${TESTS[i]} &> /dev/null";
+	exit "$?";
+    fi
+    echo "SUCCESS";
+done
