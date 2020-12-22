f912cea1628268b62cc04b3040830b309febcd6a
Charly98cma <1998cma@gmail.com>
2020-12-22 15:59:27 +0100

    Solved some issues with the 'if' statements

--- scripts/lib/serv_nisS.sh
+++ scripts/lib/serv_nisS.sh
@@ -4,8 +4,8 @@ source lib/aux_functions.sh
 nisServerFunc() {
     # Package management
     packageMng $2 "nis"
-    if [[ $? -ne 0]]; then
-    exit -1
+    if [[ $? -ne 0 ]]; then
+        exit -1
     fi
 
     # Read the parameters of the service
@@ -14,56 +14,56 @@ nisServerFunc() {
     exec 3<&-;
 
     # Check if the argument exist
-    if [[ $DOMAIN_NAME == ""]]; then
-	echoWrongParams $1 $4 $3;
-	exit 6;
+    if [[ $DOMAIN_NAME == "" ]]; then
+        echoWrongParams $1 $4 $3;
+        exit 6;
     fi
 
     # Role configuration
     sshcmd $2 "echo NISSERVER=master > /etc/default/nis"
-    if [[ $? -ne 0]]; then 
-    echoerr "\n$1 linea $4: Error al configurar el rol del servidor, no se pudo escribir en el directorio /etc/default/nis"
-    exit 40;
+    if [[ $? -ne 0 ]]; then
+        echoerr "\n$1 linea $4: Error al configurar el rol del servidor, no se pudo escribir en el directorio /etc/default/nis"
+        exit 40;
     fi
 
-    #Configure which devices have access to the NIS server
+    # Configure which devices have access to the NIS server
     sshcmd $2 "echo '255.255.255.0   10.0.0.0' >> /etc/ypserv.securenets"
-    if [[ $? -ne 0]]; then 
-    echoerr "\n$1 linea $4: Error al configurar el rango de direcciones ip que tienen acceso al servidor NIS"
-    exit 41;
+    if [[ $? -ne 0 ]]; then
+        echoerr "\n$1 linea $4: Error al configurar el rango de direcciones ip que tienen acceso al servidor NIS"
+        exit 41;
     fi
 
     # Store passwords on the NIS server too
     sshcmd $2 "sed -i 's/MERGE_PASSWD=false/MERGE_PASSWD=true/' /var/yp/Makefile"
-    if [[ $? -ne 0]]; then 
-    echoerr "\n$1 linea $4: Error al configurar el fichero /var/yp/Makefile"
-    exit 42;
+    if [[ $? -ne 0 ]]; then
+        echoerr "\n$1 linea $4: Error al configurar el fichero /var/yp/Makefile"
+        exit 42;
     fi
 
     sshcmd $2 "sed -i 's/MERGE_GROUP=false/MERGE_GROUP=true/' /var/yp/Makefile"
-    if [[ $? -ne 0]]; then 
-    echoerr "\n$1 linea $4: Error al configurar el fichero /var/yp/Makefile"
-    exit 43;
+    if [[ $? -ne 0 ]]; then
+        echoerr "\n$1 linea $4: Error al configurar el fichero /var/yp/Makefile"
+        exit 43;
     fi
 
     # Configure /etc/hosts
     sshcmd $2 "echo '$2     $DOMAIN_NAME' >> /etc/hosts"
-    if [[ $? -ne 0]]; then 
-    echoerr "\n$1 linea $4: Error al configurar /etc/hosts para añadir el servicio NIS"
-    exit 44;
+    if [[ $? -ne 0 ]]; then
+        echoerr "\n$1 linea $4: Error al configurar /etc/hosts para añadir el servicio NIS"
+        exit 44;
     fi
 
     #Update NIS database
     sshcmd $2 "/usr/lib/yp/ypinit -m < /dev/null"
-    if [[ $? -ne 0]]; then 
-    echoerr "\n$1 linea $4: Error al configurar la base de datos de NIS"
-    exit 45;
+    if [[ $? -ne 0 ]]; then
+        echoerr "\n$1 linea $4: Error al configurar la base de datos de NIS"
+        exit 45;
     fi
 
     # Start service
     sshcmd $2 "service nis restart"
-    if [[ $? -ne 0]]; then 
-    echoerr "\n$1 linea $4: Error al reiniciar el servicio NIS"
-    exit 46;
+    if [[ $? -ne 0 ]]; then
+        echoerr "\n$1 linea $4: Error al reiniciar el servicio NIS"
+        exit 46;
     fi
 }
