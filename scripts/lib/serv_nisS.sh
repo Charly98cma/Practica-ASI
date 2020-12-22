e204bed11486bec29b0c9ea512d48f3aaab3cc0a
JustAntoRS <justantorami@gmail.com>
2020-12-22 13:36:47 +0100

    Arreglados errores en el servidor NIS y añadidos codigos de error del mismo.

--- scripts/lib/serv_nisS.sh
+++ scripts/lib/serv_nisS.sh
@@ -10,11 +10,11 @@ nisServerFunc() {
 
     # Read the parameters of the service
     exec 3<> $3;
-    read HOST_NAME <&3; # Name of the NIS domain
+    read DOMAIN_NAME <&3; # Name of the NIS domain
     exec 3<&-;
 
     # Check if the argument exist
-    if [[ $HOST_NAME == ""]]; then
+    if [[ $DOMAIN_NAME == ""]]; then
 	echoWrongParams $1 $4 $3;
 	exit 6;
     fi
@@ -23,40 +23,47 @@ nisServerFunc() {
     sshcmd $2 "echo NISSERVER=master > /etc/default/nis"
     if [[ $? -ne 0]]; then 
     echoerr "\n$1 linea $4: Error al configurar el rol del servidor, no se pudo escribir en el directorio /etc/default/nis"
-    exit 10;
+    exit 40;
+    fi
 
     #Configure which devices have access to the NIS server
     sshcmd $2 "echo '255.255.255.0   10.0.0.0' >> /etc/ypserv.securenets"
     if [[ $? -ne 0]]; then 
     echoerr "\n$1 linea $4: Error al configurar el rango de direcciones ip que tienen acceso al servidor NIS"
-    exit 11;
+    exit 41;
+    fi
 
     # Store passwords on the NIS server too
     sshcmd $2 "sed -i 's/MERGE_PASSWD=false/MERGE_PASSWD=true/' /var/yp/Makefile"
     if [[ $? -ne 0]]; then 
     echoerr "\n$1 linea $4: Error al configurar el fichero /var/yp/Makefile"
-    exit 12;
+    exit 42;
+    fi
 
     sshcmd $2 "sed -i 's/MERGE_GROUP=false/MERGE_GROUP=true/' /var/yp/Makefile"
     if [[ $? -ne 0]]; then 
     echoerr "\n$1 linea $4: Error al configurar el fichero /var/yp/Makefile"
-    exit 13;
+    exit 43;
+    fi
 
     # Configure /etc/hosts
-    sshcmd $2 "echo '$2     $HOST_NAME' >> /etc/hosts"
+    sshcmd $2 "echo '$2     $DOMAIN_NAME' >> /etc/hosts"
     if [[ $? -ne 0]]; then 
     echoerr "\n$1 linea $4: Error al configurar /etc/hosts para añadir el servicio NIS"
-    exit 14;
+    exit 44;
+    fi
 
     #Update NIS database
     sshcmd $2 "/usr/lib/yp/ypinit -m < /dev/null"
     if [[ $? -ne 0]]; then 
     echoerr "\n$1 linea $4: Error al configurar la base de datos de NIS"
-    exit 15;
+    exit 45;
+    fi
 
     # Start service
     sshcmd $2 "service nis restart"
     if [[ $? -ne 0]]; then 
     echoerr "\n$1 linea $4: Error al reiniciar el servicio NIS"
-    exit 16;
+    exit 46;
+    fi
 }
